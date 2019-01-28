bc6c98f8bb62a4a75a1824a6fa755107f19b3276
Daniel Albuschat <daniel.albuschat@gira.de>
2019-01-28 10:34:14 +0100

    Improvements in the PostBuild.cmd, replaced a few hard-coded path to relative ones

--- PostBuild.cmd
+++ PostBuild.cmd
@@ -3,4 +3,4 @@ mkdir %2Output
 %2..\LogicNodesSDK\LogicNodeTool.exe updatemanifestversion %1Manifest.json %3 
 erase %2Output\%4-*.zip
 %2..\LogicNodesSDK\LogicNodeTool.exe create %1 %2Output\
-for %%i in (%2Output\%4-*.zip) DO C:\Projects\LogicNodesSDK\SignLogicNodes.exe %2..\LogicNodeCertificate.p12 "" %%i
+for %%i in (%2Output\%4-*.zip) DO %2..\LogicNodesSDK\SignLogicNodes.exe %2..\LogicNodeCertificate.p12 "" %%i
